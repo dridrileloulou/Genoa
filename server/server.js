0dea3e0c367c417406209e71ba6cd2082e1d2e47
dridrileloulou <dechivrea@gmail.com>
2026-04-15 09:23:48 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express(); // 👈 ICI app est créé
+const app = express(); 
 
 app.use(cors());
 app.use(express.json());
