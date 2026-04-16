e2c74539cd01bca33d61fa7830fc5b1c87c09c80
dgarnaud <dimitri.garnaud@bordeaux-inp.fr>
2026-04-16 09:55:32 +0200

    feat: ajout authentification JWT (login + middleware verifyToken)

---
+++ server/routes/login.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const router = express.Router(); 
+const pool = require('../db/pool.js'); // SQL
+const jwt = require('jsonwebtoken'); 
+
+const handleError = (res, err, route) => {
+    console.log(`Erreur ${route}:`, err.message);
+    res.status(400).json({ error: err.message });
+};
+
+// -- POST --
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email,password])
+    .then(result => {
+        if (result.rows.length === 0){
+            res.json(`Il n'existe pas de compte avec l'adresse mail suivante : ${email} !`);
+            console.log(`Tentative de connexion de la part de ${email}`)
+        }
+        else{
+            const token = jwt.sign({ id: result.rows[0].id, role: result.rows[0].role }, process.env.JWT_SECRET, { expiresIn: '24h' });
+            res.json({ token: token })
+            console.log(`Nouvelle connection de la part de ${email}`);
+        }
+        
+    })
+    .catch(err => handleError(res, err, 'POST /coordonnees'));
+});
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // pour stocker la clef JWT dans un .env
+
 // -- Import des Routes --
 const route_users = require('./routes/users.js');
 const route_membres = require('./routes/membres.js');
@@ -5,12 +7,22 @@ const route_unions = require('./routes/unions.js');
 const route_coordonnees = require('./routes/coordonnees.js');
 const route_logs = require('./routes/logs.js');
 const route_professions = require('./routes/professions.js');
+const route_login = require('./routes/login.js');
+const verifyToken = require('./middleware/auth.js');
+//
+
 
 const express = require('express');
 const cors = require('cors');
 const app = express(); 
 app.use(cors());
 app.use(express.json());
+
+// Attention, l'odre des routes comptes
+
+app.use(route_login)
+
+app.use(verifyToken);
 
 app.use(route_users)
 app.use(route_membres)
